8c4b1197a564e0546d3a364ae98fbddfcc94108e
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2017-09-04 21:27:40 -0400

    Add files via upload

---
+++ KG_to_LBS.py
@@ -0,0 +1,12 @@
+#Converting Weight to Pounds
+
+#Enter Weight Value
+print("Please Enter Weight")
+weightk = input()
+
+#Convert Weight to Kilos
+weight = float(weightk) * 2.20462
+
+print("Your weight in pounds is... " + str(weight))
+
+
---
+++ LBS_to_KG.py
@@ -0,0 +1,11 @@
+#Converting Weight to Kilograms
+
+#Enter Weight Value
+print("Please Enter Weight")
+weight = input()
+
+#Convert Weight to Kilos
+weightk = float(weight) * .453592
+
+print("Your weight in Kilograms is... " + str(weightk))
+
